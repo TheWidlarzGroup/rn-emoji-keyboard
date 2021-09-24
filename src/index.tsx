991322f301a985edd41212073bb155cd9a909454
KIMINJOONG <ijboym216@gmail.com>
2021-09-24 10:44:32 +0900

    add south korea language 1. add ko.ts in translation folder 2. apply ko on index.ts

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,9 @@ import { EmojiPicker } from './EmojiPicker'
 import { EmojiKeyboard } from './EmojiKeyboard'
 import en from './translation/en'
 import pl from './translation/pl'
+import ko from './translation/ko'
 
 export { EmojiKeyboard }
-export { en, pl }
+export { en, pl, ko }
 
 export default EmojiPicker
