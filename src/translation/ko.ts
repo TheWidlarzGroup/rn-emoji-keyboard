a23160d847a63f1bf160546fcf5ffa5d7e92eeb2
KimInJoong <ijboym216@gmail.com>
2021-10-01 02:19:50 +0900

    add south korea language (#22)
    1. add ko.ts in translation folder
    2. apply ko on index.ts

---
+++ src/translation/ko.ts
@@ -0,0 +1,16 @@
+import type { CategoryTranslation } from '../types'
+
+export const ko: CategoryTranslation = {
+  recently_used: '최근 사용',
+  smileys_emotion: '스마일리',
+  people_body: '사람 및 신체',
+  animals_nature: '동물 및 자연',
+  food_drink: '음식 및 음료',
+  travel_places: '여행 및 장소',
+  activities: '활동',
+  objects: '사물',
+  symbols: '기호',
+  flags: '깃발',
+  search: '검색',
+}
+export default ko
