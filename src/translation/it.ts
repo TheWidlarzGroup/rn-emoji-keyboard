d38ba6dbf9e7edf4c381b92e5af1a88e9434f717
Magda <jaskowska.magda@gmail.com>
2021-10-06 08:30:46 +0100

    Add italian translation

---
+++ src/translation/it.ts
@@ -0,0 +1,16 @@
+import type { CategoryTranslation } from '../types'
+
+export const it: CategoryTranslation = {
+  recently_used: 'Usate di recente',
+  smileys_emotion: 'Sorrisi & Emozioni',
+  people_body: 'Persone & Corpo',
+  animals_nature: 'Animali & Natura',
+  food_drink: 'Cibo & Bevande',
+  travel_places: 'Viaggi & Luoghi',
+  activities: 'Attività',
+  objects: 'Oggetti',
+  symbols: 'Simboli',
+  flags: 'Bandiere',
+  search: 'Ricerca',
+}
+export default it
