0c41ae6b5830fd1376ce397cf48c97c786da87de
Magda <73060297+mjaskowska@users.noreply.github.com>
2021-10-06 20:55:27 +0100

    Add italian translation (#23)

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,9 @@ import { EmojiKeyboard } from './EmojiKeyboard'
 import en from './translation/en'
 import pl from './translation/pl'
 import ko from './translation/ko'
+import it from './translation/it'
 
 export { EmojiKeyboard }
-export { en, pl, ko }
+export { en, pl, ko, it }
 
 export default EmojiPicker
