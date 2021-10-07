8f6dcdbfa6a1db2c53f52c3831bfbc1e508c1452
Rikky Arisendi <76026854+rikkyarisendi@users.noreply.github.com>
2021-10-07 15:29:47 +0700

    Adding Indonesia translation

---
+++ src/translation/id.ts
@@ -0,0 +1,16 @@
+import type { CategoryTranslation } from '../types'
+
+export const id: CategoryTranslation = {
+  recently_used: 'Baru saja digunakan',
+  smileys_emotion: 'Tersenyum & Emosi',
+  people_body: 'Orang & Tubuh',
+  animals_nature: 'Hewan & Alam',
+  food_drink: 'Makan & Minum',
+  travel_places: 'Perjalanam & Tempat',
+  activities: 'Aktivitas',
+  objects: 'Objek',
+  symbols: 'Simbol',
+  flags: 'Bendera',
+  search: 'Cari',
+}
+export default id
