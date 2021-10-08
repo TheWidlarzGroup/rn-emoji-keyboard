707fb6122dee4678d0a8c033e367e7d6e11f4658
Magda <73060297+mjaskowska@users.noreply.github.com>
2021-10-08 15:29:17 +0100

    Feat: Add French translation (#31)

--- README.md
+++ README.md
@@ -86,6 +86,7 @@ Example about serving as static keyboard [you can find here](/example/src/Dark/D
 Due to the limited translation possibilities, we only provide a few pre-defined translations into the following languages:
 * `en` - English 🇺🇸
 * `pl` - Polish 🇵🇱
+* `fr` - French 🇫🇷
 * `it` - Italian 🇮🇹
 * `ko` - Korean 🇰🇷
 * `id` - Indonesian 🇲🇨
--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,10 @@ import en from './translation/en'
 import pl from './translation/pl'
 import ko from './translation/ko'
 import it from './translation/it'
+import fr from './translation/fr'
 import id from './translation/id'
 
 export { EmojiKeyboard }
-export { en, pl, ko, it, id }
+export { en, pl, ko, it, fr, id }
 
 export default EmojiPicker
