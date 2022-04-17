34e517fd985f7f721e43740e6759e5ca4fb54ac8
vitaliyirtlach <w13vitaliy@gmail.com>
2022-04-17 16:48:04 +0300

    feat: Ukranian and Russia translation

--- src/index.tsx
+++ src/index.tsx
@@ -8,8 +8,10 @@ import fr from './translation/fr'
 import id from './translation/id'
 import es from './translation/es'
 import de from './translation/de'
+import ru from './translation/ru'
+import ua from './translation/ua'
 
 export { EmojiKeyboard }
-export { en, pl, ko, it, fr, id, es, de }
+export { en, pl, ko, it, fr, id, es, de, ru, ua }
 
 export default EmojiPicker
---
+++ src/translation/ua.ts
@@ -0,0 +1,17 @@
+import type { CategoryTranslation } from '../types'
+
+const ua: CategoryTranslation = {
+  smileys_emotion: 'Смайли',
+  people_body: 'Люди',
+  animals_nature: 'Тварини та природа',
+  food_drink: 'їжа та напої',
+  travel_places: 'Місця та подорожі',
+  activities: 'Заняття',
+  objects: 'Предмети',
+  symbols: 'Символи',
+  flags: 'Прапори',
+  search: 'Пошук емодзі',
+  recently_used: 'Недавні',
+}
+
+export default ua
