be23b2c76edbf4be27e00c2583e53d02bb6c38fc
vitaliyirtlach <w13vitaliy@gmail.com>
2022-04-17 16:48:04 +0300

    feat: ukranian and russian translation

---
+++ src/translation/ru.ts
@@ -0,0 +1,17 @@
+import type { CategoryTranslation } from '../types'
+
+const ru: CategoryTranslation = {
+  smileys_emotion: 'Смайлы',
+  people_body: 'Люди',
+  animals_nature: 'Животные и природа',
+  food_drink: 'Еда и напитки',
+  travel_places: 'Места и путешествия',
+  activities: 'Занятия',
+  objects: 'Предметы',
+  symbols: 'Символы',
+  flags: 'Флаги',
+  search: 'Поиск емодзи',
+  recently_used: 'Недавние',
+}
+
+export default ru
