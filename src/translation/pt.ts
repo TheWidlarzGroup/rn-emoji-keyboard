ba9d620ac2a5a95fd2430151f68517e62ced453c
urboypx <guilhermeciotapx@outlook.com>
2022-07-18 12:05:02 -0300

    feat: add portuguese translation

---
+++ src/translation/pt.ts
@@ -0,0 +1,16 @@
+import type { CategoryTranslation } from '../types'
+
+export const pt: CategoryTranslation = {
+  recently_used: 'Usado recentemente',
+  smileys_emotion: 'Sorrisos & Emoções',
+  people_body: 'Pessoas & Corpo',
+  animals_nature: 'Animais & Natureza',
+  food_drink: 'Comida & Bebibda',
+  travel_places: 'Viagens & Lugares',
+  activities: 'Atividades',
+  objects: 'Objetos',
+  symbols: 'Símbolos',
+  flags: 'Bandeiras',
+  search: 'Buscar',
+}
+export default pt
