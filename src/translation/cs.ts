ae252c7da0dad617117393a2e4a6ebc69e08ebab
pchalupa <chalupa.petr93@gmail.com>
2022-09-02 16:03:48 +0200

    feat: add czech language

---
+++ src/translation/cs.ts
@@ -0,0 +1,16 @@
+import type { CategoryTranslation } from '../types'
+
+export const cs: CategoryTranslation = {
+  recently_used: 'Naposledy použité',
+  smileys_emotion: 'Smajlíky & Emoce',
+  people_body: 'Lidé & Tělo',
+  animals_nature: 'Zvířata & Příroda',
+  food_drink: 'Jídlo & Pití',
+  travel_places: 'Cestování & Místa',
+  activities: 'Aktivity',
+  objects: 'Objekty',
+  symbols: 'Symboly',
+  flags: 'Vlajky',
+  search: 'Hledat',
+}
+export default cs
