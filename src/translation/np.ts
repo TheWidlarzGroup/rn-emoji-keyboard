1e8e0e4588695e67d8b8bc1bcf7917dde546e452
Manish Bista <31028044+manishbsta@users.noreply.github.com>
2022-11-26 22:49:04 +0545

    feat: add np translation (#101)

---
+++ src/translation/np.ts
@@ -0,0 +1,16 @@
+import type { CategoryTranslation } from '../types'
+
+const np: CategoryTranslation = {
+  recently_used: 'हालै प्रयोग गरिएको',
+  smileys_emotion: 'स्माइलीहरू र भावना',
+  people_body: 'मानिस र शरीर',
+  animals_nature: 'जनावरहरू र प्रकृति',
+  food_drink: 'खाना र पेय',
+  travel_places: 'यात्रा र स्थानहरू',
+  activities: 'गतिविधिहरू',
+  objects: 'वस्तुहरू',
+  symbols: 'प्रतीकहरू',
+  flags: 'झण्डाहरू',
+  search: 'खोज्नुहोस्',
+}
+export default np
