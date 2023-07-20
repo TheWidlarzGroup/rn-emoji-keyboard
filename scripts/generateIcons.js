f6e55d2553b51d109157d79ff887558cd57a3eef
Mateusz Bętka <37906388+mateki0@users.noreply.github.com>
2023-07-20 20:15:39 +0200

    feat: Smart search  (#146)
    Co-authored-by: Jakub Grzywacz <kontakt@jakubgrzywacz.pl>

--- scripts/generateIcons.js
+++ scripts/generateIcons.js
@@ -1,16 +1,20 @@
 const json = require('unicode-emoji-json/data-by-group.json')
 const fs = require('fs')
+const emojisWithKeywords = require('emojilib')
 
 const newArray = []
 for (const [key, value] of Object.entries(json)) {
   const newData = value
     .filter((emoji) => parseFloat(emoji.unicode_version) < 12)
-    .map((emoji) => ({
-      emoji: emoji.emoji,
-      name: emoji.name,
-      v: emoji.unicode_version,
-      toneEnabled: emoji.skin_tone_support,
-    }))
+    .map((emoji) => {
+      return {
+        emoji: emoji.emoji,
+        name: emoji.name,
+        v: emoji.unicode_version,
+        toneEnabled: emoji.skin_tone_support,
+        keywords: emojisWithKeywords[emoji.emoji],
+      }
+    })
   newArray.push({
     title: key.replace(/ & /g, '_').replace(/ /g, '_').toLocaleLowerCase(),
     data: newData,
