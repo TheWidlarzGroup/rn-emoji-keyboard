95f693c3217d1fc4ba6879a286711da0c37c1b9b
Jakub Grzywacz <kontakt@jakubgrzywacz.pl>
2024-01-31 13:57:05 +0100

    fix: remove unnecessary function

--- src/utils/deepMerge.ts
+++ src/utils/deepMerge.ts
@@ -2,14 +2,12 @@ export type RecursivePartial<T> = Partial<{
   [P in keyof T]: T[P] extends Record<string, any> ? RecursivePartial<T[P]> : T[P]
 }>
 
-const objectKeys = <T extends Record<string, any>>(obj: T) => Object.keys(obj) as (keyof T)[]
-
 export const deepMerge = <T extends Record<K, any>, K extends keyof T>(
   source: T,
   additional: RecursivePartial<T>,
 ): T => {
   const result: T = { ...source }
-  ;(objectKeys(additional) as K[]).forEach((key) => {
+  ;(Object.keys(additional) as K[]).forEach((key) => {
     if (
       key in source &&
       key in additional &&
