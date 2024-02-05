8a84845cc6d6889f49dee876f33772a547022b81
Dominik Danielewicz <ddanielewicz.dev@gmail.com>
2024-02-05 19:43:34 +0100

    feat: adding swedish translations

---
+++ src/translation/se.ts
@@ -0,0 +1,17 @@
+import type { CategoryTranslation } from '../types'
+
+const se: CategoryTranslation = {
+  recently_used: 'Senast använda',
+  smileys_emotion: 'Smileys & Känslouttryck',
+  people_body: 'Människa & Kropp',
+  animals_nature: 'Djur & Natur',
+  food_drink: 'Mat & Dryck',
+  travel_places: 'Resa & Platser',
+  activities: 'Aktiviteter',
+  objects: 'Objekt',
+  symbols: 'Symboler',
+  flags: 'Flaggor',
+  search: 'Sök',
+}
+
+export default se
